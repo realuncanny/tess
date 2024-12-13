dcac29a1a16db86c8abd7eb339a5acf3de988b44
Berke <63060680+akenshaw@users.noreply.github.com>
2024-12-13 19:04:36 +0300

    fix platform specific window configs `settings()` 

--- src/window.rs
+++ src/window.rs
@@ -105,6 +105,20 @@ where
         })
 }
 
+#[cfg(target_os = "linux")]
+pub fn settings() -> Settings {
+    use data::environment;
+    use iced::window;
+
+    Settings {
+        platform_specific: window::settings::PlatformSpecific {
+            application_id: environment::APPLICATION_ID.to_string(),
+            override_redirect: false,
+        },
+        ..Default::default()
+    }
+}
+
 #[cfg(target_os = "macos")]
 pub fn settings() -> Settings {
     use iced::window;
@@ -116,5 +130,33 @@ pub fn settings() -> Settings {
             fullsize_content_view: true,
         },
         ..Default::default()
+    }
+}
+
+#[cfg(target_os = "windows")]
+pub fn settings() -> Settings {
+    use iced::window;
+    use image::EncodableLayout;
+
+    let img = image::load_from_memory_with_format(
+        include_bytes!("../assets/logo.png"),
+        image::ImageFormat::Png,
+    );
+    match img {
+        Ok(img) => match img.as_rgba8() {
+            Some(icon) => Settings {
+                icon: window::icon::from_rgba(
+                    icon.as_bytes().to_vec(),
+                    icon.width(),
+                    icon.height(),
+                )
+                .ok(),
+                ..Default::default()
+            },
+            None => Default::default(),
+        },
+        Err(_) => Settings {
+            ..Default::default()
+        },
     }
 }
